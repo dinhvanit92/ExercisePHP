705bde3eed360ab7e07e2f86099e448077543f49
Van Tran <dinhvan.it92@gmail.com>
2021-01-17 13:05:51 +0700

    add mysql to git

--- product.sql
+++ product.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5deb2
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Jan 15, 2021 at 05:03 PM
--- Server version: 8.0.22-0ubuntu0.20.04.3
+-- Host: db
+-- Generation Time: Jan 17, 2021 at 06:04 AM
+-- Server version: 8.0.22
 -- PHP Version: 7.4.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -30,11 +29,11 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `orders` (
   `id` int NOT NULL,
-  `order_code` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `order_code` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `order_carts` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `order_date` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `user_id` int NOT NULL,
-  `total` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+  `total` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -42,16 +41,10 @@ CREATE TABLE `orders` (
 --
 
 INSERT INTO `orders` (`id`, `order_code`, `order_carts`, `order_date`, `user_id`, `total`) VALUES
-(1, 'Administratorc345bc33732dbd4a9b470c4cb921fcf0', '{\"1\":{\"id\":\"1\",\"title\":\"Tru00e0 su1eeda Tru00e2n chu00e2u\",\"price\":\"150000\",\"quantity\":\"1\",\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/ceBZsyvgyRqGWXGEfFvM.png\"},\"2\":{\"id\":\"2\",\"title\":\"Tru00e0 su1eeda xou00e0i tru00e2n chu00e2u u0111en\",\"price\":\"51000\",\"quantity\":\"1\",\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/iAnbgLkjdZ1Kud5qpzEd.png\"},\"3\":{\"id\":\"3\",\"title\":\"u0110u00e0o Hu1ed3ng Mu1eadn, Hu1ea1t u00e9\",\"price\":\"51000\",\"quantity\":\"1\",\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/So9zlHwVzXzZHeIKmx9l.png\"}}', '', 37, ''),
-(2, 'Administrator358ba1de8c4374de8d631c8eec639808', '{\"1\":{\"id\":\"1\",\"title\":\"Tru00e0 su1eeda Tru00e2n chu00e2u\",\"price\":\"150000\",\"quantity\":\"1\",\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/ceBZsyvgyRqGWXGEfFvM.png\"}}', '15-01-2121 15-01-25', 37, ''),
-(3, 'Administrator358ba1de8c4374de8d631c8eec639808', '{\"1\":{\"id\":\"1\",\"title\":\"Tru00e0 su1eeda Tru00e2n chu00e2u\",\"price\":\"150000\",\"quantity\":\"1\",\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/ceBZsyvgyRqGWXGEfFvM.png\"}}', '15-01-2121 15-01-59', 37, '150000'),
-(4, 'vantrana4f0a2c57d6c7c5ab0e0ee32a611cf08', '{\"1\":{\"id\":\"1\",\"title\":\"Tru00e0 su1eeda Tru00e2n chu00e2u\",\"price\":\"150000\",\"quantity\":\"1\",\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/ceBZsyvgyRqGWXGEfFvM.png\"},\"3\":{\"id\":\"3\",\"title\":\"u0110u00e0o Hu1ed3ng Mu1eadn, Hu1ea1t u00e9\",\"price\":\"51000\",\"quantity\":4,\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/So9zlHwVzXzZHeIKmx9l.png\"}}', '15-01-2121 15-01-07', 9, '354000'),
-(5, 'vantran17ca969bc69402d2b6ca0404f36efff4', '{\"1\":{\"id\":\"1\",\"title\":\"Tru00e0 su1eeda Tru00e2n chu00e2u\",\"price\":\"150000\",\"quantity\":2,\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/ceBZsyvgyRqGWXGEfFvM.png\"},\"\":{\"id\":null,\"title\":null,\"price\":null,\"quantity\":8,\"image\":null}}', '15-01-2121 15-01-06', 9, '300000'),
-(6, 'vantranf18a247606f7821f1cf5d0dfaf8797b8', '{\"\":{\"id\":null,\"title\":null,\"price\":null,\"quantity\":2,\"image\":null}}', '15-01-2121 15-01-34', 9, '0'),
-(7, 'vantran4719a4f79a701cd565b66ca8db5b711f', '{\"\":{\"id\":null,\"title\":null,\"price\":null,\"quantity\":\"1\",\"image\":null}}', '15-01-2121 15-01-43', 9, '0'),
-(8, 'vantran358ba1de8c4374de8d631c8eec639808', '{\"1\":{\"id\":\"1\",\"title\":\"Tru00e0 su1eeda Tru00e2n chu00e2u\",\"price\":\"150000\",\"quantity\":\"1\",\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/ceBZsyvgyRqGWXGEfFvM.png\"}}', '15-01-2121 15-01-58', 9, '150000'),
-(9, 'vantran1a48ca77ab7ef4d1530d77309cd71779', '{\"1\":{\"id\":\"1\",\"title\":\"Tru00e0 su1eeda Tru00e2n chu00e2u\",\"price\":\"150000\",\"quantity\":4,\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/ceBZsyvgyRqGWXGEfFvM.png\"},\"2\":{\"id\":\"2\",\"title\":\"Tru00e0 su1eeda xou00e0i tru00e2n chu00e2u u0111en\",\"price\":\"51000\",\"quantity\":2,\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/iAnbgLkjdZ1Kud5qpzEd.png\"},\"3\":{\"id\":\"3\",\"title\":\"u0110u00e0o Hu1ed3ng Mu1eadn, Hu1ea1t u00e9\",\"price\":\"51000\",\"quantity\":\"1\",\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/So9zlHwVzXzZHeIKmx9l.png\"}}', '15-01-2121 16-01-18', 9, '753000'),
-(10, 'vantran358ba1de8c4374de8d631c8eec639808', '{\"1\":{\"id\":\"1\",\"title\":\"Tru00e0 su1eeda Tru00e2n chu00e2u\",\"price\":\"150000\",\"quantity\":\"1\",\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/ceBZsyvgyRqGWXGEfFvM.png\"}}', '15-01-2121 16-01-52', 9, '150000');
+(25, 'vantran05932ffffda04f58d45917eeab91d257', '{\"1\":{\"id\":\"1\",\"title\":\"Trà sửa Trân châu\",\"price\":\"150000\",\"quantity\":\"1\",\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/ceBZsyvgyRqGWXGEfFvM.png\"},\"2\":{\"id\":\"2\",\"title\":\"Trà sửa xoài trân châu đen\",\"price\":\"51000\",\"quantity\":\"1\",\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/iAnbgLkjdZ1Kud5qpzEd.png\"}}', '17-01-2121 05-01-25', 9, '201000'),
+(26, 'vantran3c72258059461a9c2defc158ae6d15f7', '{\"1\":{\"id\":\"1\",\"title\":\"Trà sửa Trân châu\",\"price\":\"150000\",\"quantity\":\"3\",\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/ceBZsyvgyRqGWXGEfFvM.png\"}}', '17-01-2121 05-01-21', 9, '450000'),
+(27, 'vantran7630fcbb4124450da0eb161e9b5af784', '{\"2\":{\"id\":\"2\",\"title\":\"Trà sửa xoài trân châu đen\",\"price\":\"51000\",\"quantity\":\"1\",\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/iAnbgLkjdZ1Kud5qpzEd.png\"}}', '17-01-2121 05-01-22', 9, '51000'),
+(28, 'vantran923d99d3327eae6bfbdca9cc3714cce3', '{\"1\":{\"id\":\"1\",\"title\":\"Trà sửa Trân châu\",\"price\":\"150000\",\"quantity\":\"2\",\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/ceBZsyvgyRqGWXGEfFvM.png\"},\"2\":{\"id\":\"2\",\"title\":\"Trà sửa xoài trân châu đen\",\"price\":\"51000\",\"quantity\":\"3\",\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/iAnbgLkjdZ1Kud5qpzEd.png\"},\"3\":{\"id\":\"3\",\"title\":\"Đào Hồng Mận, Hạt é\",\"price\":\"51000\",\"quantity\":\"4\",\"image\":\"http://candyshop.trandinhvan.com/storage/product/1/So9zlHwVzXzZHeIKmx9l.png\"}}', '17-01-2121 05-01-59', 9, '657000');
 
 -- --------------------------------------------------------
 
@@ -61,10 +54,10 @@ INSERT INTO `orders` (`id`, `order_code`, `order_carts`, `order_date`, `user_id`
 
 CREATE TABLE `products` (
   `id` int NOT NULL,
-  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `description` text COLLATE utf8_unicode_ci NOT NULL,
-  `price` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `price` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `image` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -84,9 +77,9 @@ INSERT INTO `products` (`id`, `title`, `description`, `price`, `image`) VALUES
 
 CREATE TABLE `users` (
   `id` int NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -184,7 +177,7 @@ ALTER TABLE `user_products`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `products`
